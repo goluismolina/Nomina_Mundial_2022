d812efae632e9ec75060dfb2a5b27cc0ee90bc18
Luis Molina <luisemr14molina@gmail.com>
2022-09-12 00:32:30 -0300

    Tercer commit

--- Equipos/Pages/Test.cshtml.cs
+++ Equipos/Pages/Test.cshtml.cs
@@ -5,8 +5,21 @@ namespace Equipos.Pages
 {
     public class TestModel : PageModel
     {
+        [BindProperty]
+        public int Numero1 { get; set; }
+        [BindProperty]
+        public int Numero2 { get; set; }
+
+        public int Resultado { get; set; }
         public void OnGet()
         {
+            //this.Numero1 = 123;
+            //this.Numero2 = 321;   
+        }
+        public void OnPost()
+        {
+            var resultado = this.Numero1 + this.Numero2;
+            this.Resultado = resultado;
         }
     }
 }
